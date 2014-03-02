158983cf683ebc21541489cbe0394c9ebf62a241
stanshch <stanshch@gmail.com>
2014-03-02 11:42:25 -0500

    version 4

--- tip_calculator.py
+++ tip_calculator.py
@@ -1,15 +1,21 @@
-import sys
+from optparse import OptionParser
 
-meal = float(sys.argv[1])
-tax = float(sys.argv[2])/100
-tip = float(sys.argv[3])/100
+parser = OptionParser()
+parser.add_option("-m", "--meal", dest="meal", help="pre-tax cost of the meal", type="float")
+parser.add_option("-x", "--tax", dest="tax", help="the tax rate in the 0.XX format", type="float")
+parser.add_option("-p", "--tip", dest="tip", help="your tip in the 0.XX format", type="float", default=0.20)
 
-tax_value = meal*tax
-meal_with_tax = meal + tax_value
-tip_value = meal_with_tax*tip
+(options, args) = parser.parse_args()
+
+if not (options.meal and options.tax):
+	parser.error("You need to supply both the pre-tax meal and tax rate")
+
+tax_value = options.meal*options.tax
+meal_with_tax = options.meal + tax_value
+tip_value = meal_with_tax*options.tip
 total = meal_with_tax+tip_value
 
-print "The base cost of the meal is $%1.2f" %meal
+print "The base cost of the meal is $%1.2f" %options.meal
 print "Your tax is $%1.2f" %tax_value
 print "The tip you will pay is $%1.2f" %tip_value
 print "Your grand total is $%1.2f" %total
